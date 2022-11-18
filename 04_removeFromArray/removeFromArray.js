6ba4b5ce03064ec19f5dc9112113cb5241dd98a2
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-18 22:09:14 +0100

    ex 4 first step done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray,deleteIt) {
 
+    const index = givenArray.indexOf(deleteIt);
+    if (index > -1) { // only splice array when item is found
+        givenArray.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    return givenArray
 };
 
 // Do not edit below this line
