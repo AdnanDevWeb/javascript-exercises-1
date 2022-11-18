f0a0461bce1cd7a8cc45d6f289962f9185432ba3
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-18 21:09:53 +0100

    ex 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = string.split("")
+    reversedString.reverse();
+    let output="";
+    for (let index = 0; index < reversedString.length; index++) {
+        output+=reversedString[index]
+    }
+    return output
 };
 
 // Do not edit below this line
