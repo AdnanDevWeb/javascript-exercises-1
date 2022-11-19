f4912eed50b5e8a102bf2f248c512e37a4f60f23
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-19 22:53:35 +0100

    valided first step after 1h

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start , end){
+    for(let i = 0 ; i<arguments.length ; i++){
+        if(arguments[i] === NaN || typeof arguments[i]  === String || arguments[i] < 0){
+            return undefined;
+        }
+    }
+    let count = start
+    let sum = 0;
+    let finalSum = 0
+    for(let i = 0 ; i < end ; i++){
+        sum = count + i;
+        finalSum+=sum
+    }
+    return finalSum
 };
 
 // Do not edit below this line
