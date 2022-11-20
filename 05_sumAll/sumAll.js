bb8cafef5b9ef9dbbb122fb2fe2b35576f9d4a7c
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-20 19:55:29 +0100

    ex 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,29 @@
 const sumAll = function(start , end){
     for(let i = 0 ; i<arguments.length ; i++){
-        if(arguments[i] === NaN || typeof arguments[i]  === String || arguments[i] < 0){
-            return undefined;
+        if(isNaN(arguments[i]) === true || arguments[i] < 0 || typeof arguments[i] == 'string'){
+            return "ERROR";
         }
     }
-    let count = start
     let sum = 0;
-    let finalSum = 0
-    for(let i = 0 ; i < end ; i++){
-        sum = count + i;
-        finalSum+=sum
+    let finalSum = 0;
+    let count;
+    if(start < end){
+        count = start
+        for(let i = 0 ; i < end ; i++){
+            sum = count + i;
+            finalSum+=sum
+        }
+
     }
+    else{
+        count = end;
+        for(let i = 0 ; i < start ; i++){
+            sum = count + i;
+            finalSum+=sum
+        }
+    }
+    
+    
     return finalSum
 };
 
