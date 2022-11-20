9ac83d2639a86e1c11b085fa078f06ffa2f9b86f
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-20 22:16:11 +0100

    ex 7 chechpoint ( temp conversion )

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(value) {
+  const toRound = (value - 32) * 5/9
+  return Math.round(toRound)
 };
 
-const ctof = function() {
-
+const ctof = function(value) {
+  const toRound = (value * 9/5) + 32
+  return Math.round(toRound)
 };
 
 // Do not edit below this line
