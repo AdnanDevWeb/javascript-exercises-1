55e318af79749fa2b0ce7c3e66c6763539820e00
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-21 20:04:48 +0100

    ex 7 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(val1 , val2) {
+  return val1 + val2
 };
 
-const subtract = function() {
-	
+const subtract = function(val1 , val2) {
+	return val1 - val2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summmed = 0;
+	for(let i =0 ; i<array.length ; i++){
+    summmed+=array[i]
+  }
+  return summmed
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let multiplied = 1;
+	for(let i =0 ; i<array.length ; i++){
+    multiplied*=array[i]
+  }
+	return multiplied
+};
+
+const power = function(val1,val2) {
+  return Math.pow(val1,val2)
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  } 
 };
 
 // Do not edit below this line
